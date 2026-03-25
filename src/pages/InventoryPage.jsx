09ffd019cea793e5a5b374e9f5a343ff18a0baea
superquique <enriqueveintidos@atoms.mx>
2026-03-25 02:34:45 -0600

    refactor: change api calls to use crud.js functions

--- src/pages/InventoryPage.jsx
+++ src/pages/InventoryPage.jsx
@@ -9,6 +9,11 @@ function InventoryPage () {
     const [productList, setProductList] = useState([]);
     const [activeStorage, setActiveStorage] = useState(null);
 
+    const fetchProducts = async () => {
+        const products = await getProducts();
+        setProductList(products);
+    }
+
     const displayedProducts = useMemo(() => {
         if (activeStorage !== null) {
             return productList.filter(p => p.storage === activeStorage);
@@ -25,15 +30,6 @@ function InventoryPage () {
         }
     }
 
-    const fetchProducts = async () => {
-        const products = await getProducts();
-        setProductList(products);
-    }
-
-    useEffect(() => {
-        fetchProducts();
-    }, [])
-
     const onAddProduct = async () => {
         const newProductDetails = {
             "name": "",
@@ -42,8 +38,8 @@ function InventoryPage () {
             "store": "",
             "quantity": 1,
             "needed": 0,
+            "automatic_restock": 0,
             "usual": false,
-            "restock": false,
             "note": "",
             "storage": ""
         }
@@ -54,15 +50,29 @@ function InventoryPage () {
         }
     }
 
-
     const onChange = async (id, value, field) => {
         // Update product with new field value
-        console.log(id, value, field);
         const update = {
             [field]: value
         }
 
-        console.log(update);
+        const result = await updateProduct(id, update);
+        if (result.status === 200) {
+            fetchProducts();
+        }
+    }
+
+    const onChangeQuantity = async (id, newQuantity) => {
+        console.log(id, newQuantity);
+        const product = productList.find(product => product.id === id);
+        const update = {
+            quantity: Math.max(newQuantity, 0)
+        }
+
+        if (update.quantity === 0 && product.automatic_restock > 0) {
+            update.needed = product.automatic_restock
+        }
+
         const result = await updateProduct(id, update);
         console.log(result);
         if (result.status === 200) {
@@ -77,6 +87,10 @@ function InventoryPage () {
             fetchProducts();
         }
     }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
 
     if (productList === null) {
         return (
@@ -100,6 +114,7 @@ function InventoryPage () {
                                 key={product.id}
                                 productDetails={product} 
                                 onChange={onChange}
+                                onChangeQuantity={onChangeQuantity}
                                 onDelete={onDelete}
                             />
                         )
